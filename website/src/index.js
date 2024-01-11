5816a820dd4ab891eec9e1c036635438b135eb6b
Finebouche <tanguy.cazalets@protonmail.com>
2024-01-11 21:18:13 +0700

    reverse to express

--- website/src/index.js
+++ website/src/index.js
@@ -1,9 +1,6 @@
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  async function handleRequest(request) {
-    return new Response('Hello worker!', {
-      headers: { 'content-type': 'text/plain' },
-    })
-  }
+const app = require('./app');
+const port = 4000
+
+// Start the server
+app.listen(port, () => console.log(`The server is listening on port ${port}`))
+
